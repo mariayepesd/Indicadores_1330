86cead82a4a027ff4c40f659c3b3aab5c801302a
DiegoGo001 <109126896+DiegoGo001@users.noreply.github.com>
2023-10-21 02:11:49 -0500

    Cambios controladores

--- controlador/ControlResultadoIndicador.php
+++ controlador/ControlResultadoIndicador.php
@@ -17,5 +17,43 @@
             $objControlConexion->cerrarBd();
         }
 
+
+        function listarResultadoIndicador($id){
+            $comandoSql = "SELECT resultadoindicador.id,indicador.nombre 
+            FROM resultadoindicador INNER JOIN indicador ON resultadoindicador.fkidindicador = indicador.id
+            WHERE id = '$id'";
+            $objControlConexion = new ControlConexion();
+            $objControlConexion->abrirBd($GLOBALS['serv'], $GLOBALS['usua'], $GLOBALS['pass'], $GLOBALS['bdat'], $GLOBALS['port']);
+            $recordSet = $objControlConexion->ejecutarSelect($comandoSql);
+            if (mysqli_num_rows($recordSet) > 0) {
+                $arregloResultado = array();
+                $i = 0;
+                while($row = $recordSet->fetch_array(MYSQLI_BOTH)){
+                    $objResultadoIndicador = new Rol(0,"");
+                    $objResultadoIndicador->setId($row['id']);
+                    $objResultadoIndicador->setNombre($row['resultado']);
+                    $objResultadoIndicador[$i] = $objResultadoIndicador;
+                    $i++;
+                }
+            }
+            $objControlConexion->cerrarBd();
+            return $arregloResultado;
+        }
+
+        function borrar(){
+            $fkIdresultado= $this->objResultadoIndicador->getfkIdresultado(); 
+            $fkIdIndicador = $this->objResultadoIndicador->getfkIdIndicador();
+            $comandoSql = "DELETE FROM resultadoindicador WHERE fkIdresultado = '$fkIdresultado' AND fkIdIndicador = '$fkIdIndicador'";
+            $objControlConexion = new ControlConexion();
+            $objControlConexion->abrirBd($GLOBALS['serv'],$GLOBALS['usua'],$GLOBALS['pass'],$GLOBALS['bdat'],$GLOBALS['port']);
+            $objControlConexion->ejecutarComandoSql($comandoSql);
+            $objControlConexion->cerrarBd();
+        }
+
+
+
+
+
+
     }
 ?>
--- modelo/Variable.php
+++ modelo/Variable.php
@@ -7,11 +7,12 @@ class Variable {
         var $fechaCreacion;
         var $fkEmailUsuario;
 
-        public function __construct($id, $nombre) {
+        public function __construct($id, $nombre,$fechaCreacion,$fkEmailUsuario) {
             
             $this->id = $id;
             $this->nombre = $nombre;
-
+            $this->fechaCreacion = $fechaCreacion;
+            $this->fkEmailUsuario = $fkEmailUsuario;
         }
 
 
